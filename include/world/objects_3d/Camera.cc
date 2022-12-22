5ee50c7de98ff17a0951265d3604599f9fd9f7ec
TomasZlamal <107461326+TomasZlamal@users.noreply.github.com>
2022-12-22 16:13:17 +0100

    Delete Camera.cc

--- include/world/objects_3d/Camera.cc
+++
@@ -1,7 +0,0 @@
-#include "Camera.h"
-namespace r3d{
-Camera::Camera(r3d::point3d camera_pos, r3d::point3d camera_direction, 
-              r3d::point3d camera_up)
-  : m_camera_pos(camera_pos), m_camera_direction(camera_direction),
-    m_camera_up(camera_up){}
-}
